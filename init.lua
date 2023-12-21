ad19693aca1abdd68fcb60133b0221bf3a41b384
Magomed Shamaev <ms77grz@gmail.com>
2023-12-21 14:16:53 +0300

    Add init.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+vim.opt.colorcolumn = "80"
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
+vim.api.nvim_set_keymap('i', 'JK', '<Esc>', {noremap = true})
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.scrolloff = 999
